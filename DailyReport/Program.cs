a5d2fa44a4c6723a2deaa802edf541f8e22030e3
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-16 14:48:55 -0600

    Add Daily Report Project

---
+++ DailyReport/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pgNumber = Convert.ToInt32(pageNumber);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string helpRequired = Console.ReadLine();
+            bool help = Convert.ToBoolean(helpRequired);
+            Console.WriteLine("Were there any positive experiences you\'d like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            byte hrsStudied = Convert.ToByte(hoursStudied);
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
