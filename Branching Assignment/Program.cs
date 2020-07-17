5cf9baef6a8080e574f6ceb31b03b37ebf1fc74f
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-17 11:13:46 -0600

    Add Branching Assignment

---
+++ Branching Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int totalDimension = width + height + length;
+            decimal quote = ((width * height * length) * weight) / 100;
+
+            string result = totalDimension > 50 ? "Your package is too large to ship via Package Express." : "Your estimated total for shipping this package is: $" + quote +".00";
+            Console.WriteLine(result);
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+
+        }
+    }
+}
