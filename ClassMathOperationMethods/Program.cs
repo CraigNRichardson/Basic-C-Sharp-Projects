8e27d2dae92a34fdbb069d1ad880c6e0267b6322
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-21 13:12:26 -0600

    Add Class Methods Assignment

---
+++ ClassMathOperationMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMathOperationMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Operations operations = new Operations();
+            Console.WriteLine("The area of a circle with a radius of " + num + " is: " + operations.AreaCircle(num) + ".");
+            Console.WriteLine("The volume of a sphere with a radius of " + num + " is: " + operations.VolumeSphere(num) + ".");
+            Console.WriteLine("The volume of a cube with a segment length of " + num + " is: " + operations.VolumeCube(num) + ".");
+
+            Console.ReadLine();
+
+
+            
+
+            
+
+        
+        
+            
+        }
+    }
+}
