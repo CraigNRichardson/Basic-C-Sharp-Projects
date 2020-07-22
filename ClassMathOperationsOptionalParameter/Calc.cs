4ef74fa2eda4653e248e9ad8e42411cb839afecf
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-21 21:14:18 -0600

    Add Optional Parameter Exercise

---
+++ ClassMathOperationsOptionalParameter/Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMathOperationsOptionalParameter
+{
+    class Calc
+    {
+        public Calc()
+        {
+
+        }
+
+        public int Add(int num, int num2 = 5)
+        {
+            return num + num2;
+        }
+
+    }
+}
