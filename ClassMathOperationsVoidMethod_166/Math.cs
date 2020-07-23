7cc32f08a0067aaeca862375b2820894fac5f709
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-23 13:27:49 -0600

    Add Void Method Assignment 166

---
+++ ClassMathOperationsVoidMethod_166/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMathOperationsVoidMethod_166
+{
+    public class Math
+    {
+        
+        int num1, num2 = 4;
+
+        public void Math1()
+        {
+            
+            Console.WriteLine("The Second number is: " + num2);
+        }
+
+        public void Math2()
+        {
+            this.num1 = 3;
+            int num3 = num1 * num2;
+            Console.WriteLine("The Result is {0}", num3);
+        }
+
+        
+    }
+}
---
+++ ClassMathOperationsVoidMethod_166/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMathOperationsVoidMethod_166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Math1();
+            math.Math2();
+            Console.ReadLine();
+
+        }
+
+        
+    }
+
+    
+}
