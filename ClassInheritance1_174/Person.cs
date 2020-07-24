28b026ee2a6aed3fd1548c750a797d4bb27d1060
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-24 10:36:52 -0600

    Add Class Inheritance Assignment 174

---
+++ ClassInheritance1_174/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance1_174
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + fullName);
+        }
+    }
+
+}
