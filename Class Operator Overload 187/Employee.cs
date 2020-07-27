75ae562669afe076db714278aca082e8523d1469
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-27 14:40:43 -0600

    Add Operator Overload Assignment 187

---
+++ Class Operator Overload 187/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritanceInterface_182
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + fullName);
+        }
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.Id == employee2.Id)
+            {
+                status = true;
+            }
+
+            if (employee1.Id != employee2.Id)
+            {
+                status = false;
+            }
+            return status;
+            
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.Id != employee2.Id)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Employee has quit.");
+
+        }
+    }
+}
---
+++ Class Operator Overload 187/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassInheritanceInterface_182;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Operator_Overload_187
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Bill";
+            employee1.LastName = "Smith";
+            employee1.Id = 1;
+            Employee employee2 = new Employee();
+            employee2.FirstName = "William";
+            employee2.LastName = "Smith";
+            employee2.Id = 2;
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("This is the same employee.");
+            }
+            else
+            {
+                Console.WriteLine("These are different employees.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
