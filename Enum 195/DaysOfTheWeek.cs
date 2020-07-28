7c906f5a16b6b0c60c91cdf544131120706071bc
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-28 11:42:50 -0600

    Add Enum 195 Assignment

---
+++ Enum 195/DaysOfTheWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_195
+{
+    class DaysOfTheWeek
+    {
+        public enum Days
+        {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7
+        }
+    }
+}
