f9f79dc8ff77ea5b34b60a814b867a48dcc56ca6
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-28 00:11:43 -0600

    Add Class Generic Type Parameter 190

---
+++ Class Generic Type Parameter 190/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Generic_Type_Parameter_190
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void SayName();
+
+    }
+}
