14cf75499efba5f54f1e1c21b0c1373dcec9b3ac
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-30 10:46:43 -0600

    Add File IO Assignment 219

---
+++ File IO 219/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number: ");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Craig\Desktop\C#Projects\Logs\log.txt", text);
+            string textReturn = File.ReadAllText(@"C:\Users\Craig\Desktop\C#Projects\Logs\log.txt");
+            Console.WriteLine(textReturn);
+            Console.ReadLine();
+
+        }
+    }
+}
